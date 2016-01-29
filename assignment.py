9cf448e1f2cf5783b7bffc3247082df22f424e0a
cmis <cmis@cmis-hs16.(none)>
2016-01-29 10:55:04 +0700

    Still in process

---
+++ assignment.py
@@ -0,0 +1,23 @@
+myName = "Harry.W"
+print myName
+
+myAge = "15"
+print myAge
+
+myNationality = "Taiwan"
+print myNationality
+
+myFavGame = "NBA2K16"
+print myFavGame
+
+myHeight = "168cm"
+print myHeight
+
+myLaptop = "Asus X450JK"
+print myLaptop
+
+myLife = "Alright"
+print myLife
+
+myMood = "Normal"
+print myMood
