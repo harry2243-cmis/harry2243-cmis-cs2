4d9340b6cbf37195b76eaac34e721cd284d3ca12
cmis <cmis@cmis-hs16.(none)>
2016-02-03 08:41:17 +0700

    Complete

---
+++ assignment1.py
@@ -0,0 +1,45 @@
+myName = "Harry.W"
+print myName
+#This shows my name.
+myAge = 15 + 31/365.0
+print myAge
+#This shows my age.
+myHeight = 1.68
+print myHeight
+#This shows my height.
+sq1side = 3
+print sq1side
+#This shows the size of one side of square.
+lengthrec = 10
+print lengthrec
+#This shows the length of a rectangle.
+heightrec = 20
+print heightrec
+#This shows the height of a rectangle.
+ageinmonth = myAge * 12
+print ageinmonth
+#This shows my age, but in month.
+ageIcanlive = 80 - myAge
+print ageIcanlive
+#This shows the number of years I can still live.
+heightinfeet = myHeight * 3.28084
+print heightinfeet
+#This shows my height in feet.
+differentheiwithavghei = 1.71 - myHeight
+print differentheiwithavghei
+#This shows the difference of my height and the average height of Taiwanese.
+sq1sideaddall = sq1side ** 2
+print sq1sideaddall
+#This shows and calculates all sides of sqare added together.
+halfvolume = (lengthrec ++ 2 + heightrec) / 2.0
+print halfvolume
+#This shows the half of a volume.
+print	"Hello world, my name is " + str ( myName ) + " and I'm " + str( myAge ) + " years old. My height is " + str( myHeight ) + ", in feet, it is " + str( heightinfeet ) + ". The calculation says that I still got " + str( ageIcanlive ) + " years to live. "
+#This shows the text that I typed including five variables I've created.
+print	"Hello, my age in month is " + str (ageinmonth) + ". The difference of my height and the average height in Taiwanese is " + str (differentheiwithavghei) + ". The length of a rectangle is " + str (lengthrec) + ", and the height of it is " + str (heightrec) + ". One side of square is " + str (sq1side) + "."
+#This shows the text that I typed including five variables I've created.
+print ";)" * 10000
+#This shows 10000 winking smiley faces.
+
+
+
