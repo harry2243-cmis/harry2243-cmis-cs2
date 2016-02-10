ac33e70066c95c2168b27f97e4e469ffd322bd58
cmis <cmis@cmis-hs16.(none)>
2016-02-10 10:54:07 +0700

    still in work

---
+++ functions.py
@@ -0,0 +1,12 @@
+def add(a, b):
+	return a + b
+print add(3, 4)
+def sub(a, b):
+	return a - b
+print sub(5, 3)
+def mul(a, b):
+	return a * b
+print mul(4, 4)
+def div(a, b):
+	return float(a) / b
+print div(2, 3)
