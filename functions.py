13c32ab544cd7714d92bf05eb62597cee9896dad
cmis <cmis@cmis-hs16.(none)>
2016-02-11 13:31:29 +0700

    Still in work

--- functions.py
+++ functions.py
@@ -1,12 +1,31 @@
+import math 
+
 def add(a, b):
 	return a + b
 print add(3, 4)
+
 def sub(a, b):
 	return a - b
 print sub(5, 3)
+
 def mul(a, b):
 	return a * b
 print mul(4, 4)
+
 def div(a, b):
 	return float(a) / b
 print div(2, 3)
+
+def div(a, b):
+	return a / b
+print div(86400, 3600)
+
+def mul(a, b):
+	return math.pi* a **2
+print mul(5,2)
+
+def volume(a):
+	return (((4/3.0)*math.pi)*a**3)
+print volume(5)
+
+
