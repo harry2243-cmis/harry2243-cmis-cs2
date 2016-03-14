08ba46809a3649eb616b48a741ed11c5115fd0e7
cmis <cmis@cmis-hs16.(none)>
2016-03-14 10:56:03 +0700

    working

---
+++ oneguess.py
@@ -0,0 +1,16 @@
+import random
+
+def main():
+number1 = raw_input("What is the minimum number?")
+number2 = raw_input("What is the maximum number?")
+thinkingNumber = raw_input("What do you think the number is?")
+return output()
+
+def output():
+print """
+The first target number is 2.
+The second target number is 8.
+Your guess was 5.
+ 
+
+
