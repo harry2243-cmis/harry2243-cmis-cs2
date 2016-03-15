c9fc94ec73671bd4d42898debf35ab5814d9f3d5
cmis <cmis@cmis-hs16.(none)>
2016-03-15 13:35:21 +0700

    Still in work

--- simpleprogram.py
+++ simpleprogram.py
@@ -1,6 +1,12 @@
 def main():
 
-name = raw_input("What is your name?: ")
-from = raw_input("Where are you from?: ")
-y = raw_input("Which school do you attend?: ")
+	name = raw_input("What is your name?: ")
+	whereAttend = raw_input("Which school do you attend?:")
+	firstGame = raw_input("How much points, assists, and rebounds did you score in your game 2 games ago?: ")
+	secondGame = raw_input("How much points, assists, and rebounds did you score in your latest game?: ")
+	return output()
+
+def output():
+	print """
+Greetings, Mr./Miss {}, welcome to BallDream Calculating Center. You currently attend {}. You average around {} points, {} assists, and {} rebounds from your recent 2 games.
 
