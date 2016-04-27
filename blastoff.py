3eefb3a4503d206f0165ed8fe983891becd4c661
cmis <cmis@cmis-hs06.(none)>
2016-04-27 10:08:59 +0700

    Still In Work

--- blastoff.py
+++ blastoff.py
@@ -1,14 +1,30 @@
-def countdown_from_to(start, stop):
-	if start<stop:
-		print 'Blastoff'
-	else: 
-		print start
-		countdown_from_to(start-1, stop)
+#def countdown(n):
+#	if n<=0:	
+#		print "blastoff"
+#	else:
+#		print n
+#		countdown(n-1)
+#	return
+#
+#
+#def main():
+#
+#	countdown(10)
+#
+#main()
 
+def adder():
 
-def main():
+	n0 = float(raw_input("Next Number:"))
+	if n0 != (""):
+		adder()
+	else: running_total = raw_input("0")
+		
+adder()
 
-	
-	countdown_from_to(7, 2)
+def running_total(total):
+	total = "n0"
+	total + "n0"
+	print total
 
-main()
+running_total()
