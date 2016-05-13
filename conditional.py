7f95f34934e1cd3b593fa0e7c6a1e145094b9925
cmis <cmis@cmis-hs06.(none)>
2016-05-13 08:48:19 +0700

    Work in progress

--- conditional.py
+++ conditional.py
@@ -1,43 +1,74 @@
-Name = raw_input("Contestant's name")
-print "Welcome to BallDream Calculating Center once again! Today, we present to you, The Road To The Final"
-print ("\n")
+import random
 
-print "Hello " + str(Name) + ", you are about to enter BallDream Calculating Center's The Road To The Final contest."
-print ("\n")
+def Rookie():
 
-print "The program will be testing your basketball knowledges, including thr NBA. So, without further ado, let's begin!"
+	print "Welcome to BallDream Calculating Center once again! Today, we present to you, The Road To The Final."
+	
 
-print ("\n")
+	print "Hello, you are about to enter BallDream Calculating Center's The Road To The Final contest."
+	print ("\n")
 
-print "Q.1 : Who is the father/creator, of basketball? James Naismith, or Walter Camp."
+	print "The program will be testing your basketball knowledges, including thr NBA. So, without further ado, let's begin!"
 
-print ("\n")
+	print ("\n")
 
-choice_1 =raw_input("Please type your choice here ")
+	print "Q.1 : Who is the father/creator, of basketball?"
+	print " James Naismith, or Walter Camp?"
 
-print("\n")
+	print ("\n")
 
-if choice_1 == "James Naismith":
-	print "You are correct! James Naismith invented the sport of basketball in 1891. He wrote the original basketball rulebook, founded the University of Kansas basketball program, and lived to see basketball adopted as an Olympic demonstration sport in 1904 and as an official event at the 1936 Summer Olympics in Berlin, as well as the birth of both the National Invitation Tournament and the NCAA Men's Division I Basketball Championship."
-else: 
-	print "Sorry, you have been eliminated from the contest. Better luck next time!"
-	exit()
+	choice_1 =raw_input("Please type your choice here ")
+	
+	print("\n")
 
-print ("\n")
+	if choice_1 == "James Naismith":
+		print "You are correct! James Naismith invented the sport of basketball in 1891."
+	else: 
+		print "Sorry, you are wrong~~~~ Better luck next time!"
+		exit()
+	print ("\n")
+	
 
-print "You hear a load roar close to you."
-print "Do you go investigate the noise, or do you just stay?"
+def Allstar():
 
-choice_2 = raw_input("stay, or go ")
+	print "Q.2 : What foul do you call, when a player carried the ball, instead of dribbling the ball?"
+	print "Double drible, or travel?"
 
-if choice_2 == "stay":
-	print "great choice"
-else:
-	print "You go over to investigate, you find out it's a large lion roaring, it attacks you and kills you." " You ded sorry m8"
+	choice_2 = raw_input("Please type your choice here ")
+
+	if choice_2 == "travel":
+		print "You are correct!"
+	else:
+		print "Sorry, you are wrong~~~~ Better luck next time!"
 	quit()
 
-print("\n")
+	print("\n")
+	
 
-print "You start to feel hungry, you see some roots on the ground, do you eat it or not?"
+def Halloffame():
+	
+	print "Q.3 : Who was drafted first in the 2015 NBA Draft?"
+	print "Karl Anthony Towns, or Andrew Wiggins?"
 
-choice_3 = raw_input("eat or not")
+	choice_3 = raw_input("Please type your choice here")
+
+	if choice_3 == "Karl Anthony Towns":
+		print "You are correct!"
+	else:
+		print "Sorry, you are wrong~~~~ Better luck next time!"
+		exit()
+
+		print("\n")
+
+def End():
+
+	print "Wow, you did it!"
+	print "Congradulation! You have passed the BallDream Calculating Center, The Road To The Final program!"
+	print("\n")
+	
+def main():
+	Rookie()
+	Allstar()
+	Halloffame()
+	End()
+main()	
