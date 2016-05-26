3eb81af3bc3f1f6e25a26eb0cf61725625140c70
cmis <cmis@cmis-hs06.(none)>
2016-05-26 08:38:46 +0700

    Working on it

---
+++ trytry.py
@@ -0,0 +1,28 @@
+def countdown(x):
+	while x >=0:
+		print x
+		x-=1
+
+def countup(x):
+	while x<10:
+		print x
+		x+=1
+def count(x):
+	while x < 0:
+		print x
+		x+=1
+	while x >0:
+		print x
+		x-=1
+
+def addodd():
+	while n<9:
+		print n
+		n+=1
+
+def main():
+	countdown(7)
+	countup(2)
+	count(4)
+main()
+
